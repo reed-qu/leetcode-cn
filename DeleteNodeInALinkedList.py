22869dffd7bb076fe40a33fc21fbfc2b4835b1e1
reedwave <reed_qu@qq.com>
2019-11-12 15:57:03 +0800

    237. 删除链表中的节点

---
+++ DeleteNodeInALinkedList.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/11/12 下午3:05
+# @Title   : 237. 删除链表中的节点
+# @Link    : https://leetcode-cn.com/problems/delete-node-in-a-linked-list/
+
+
+QUESTION = """
+请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+
+现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+
+示例 1:
+输入: head = [4,5,1,9], node = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+示例 2:
+输入: head = [4,5,1,9], node = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+
+说明:
+链表至少包含两个节点。
+链表中所有节点的值都是唯一的。
+给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+不要从你的函数中返回任何结果。
+"""
+
+
+THINKING = """
+删除该节点之后，该节点的值为下一节点的值，该节点的下一节点为下下个节点
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteNode(self, node: ListNode):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val, node.next = node.next.val, node.next.next
+
+
+if __name__ == '__main__':
+    pass
