0cb7fb843c43fc7c8ae0557c379e69f45d11b605
reedwave <reed_qu@qq.com>
2019-12-30 18:14:03 +0800

    39. 组合总和 | #15

--- CombinationSum.py
+++ CombinationSum.py
@@ -6,7 +6,8 @@
 
 
 QUESTION = """
-给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+给定一个无重复元素的数组 candidates 和一个目标数 target 
+找出 candidates 中所有可以使数字和为 target 的组合。
 candidates 中的数字可以无限制重复被选取。
 
 说明：
