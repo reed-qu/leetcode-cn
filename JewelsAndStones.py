71e13f209efe44d754ffa68bd1406ccd6de098ac
reedwave <reed_qu@qq.com>
2020-01-01 22:46:12 +0800

    771. 宝石与石头 | #70

---
+++ JewelsAndStones.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/1 下午10:39
+# @Title   : 771. 宝石与石头
+# @Link    : https://leetcode-cn.com/problems/jewels-and-stones/
+
+
+QUESTION = """
+给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 
+
+S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+
+J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+
+示例 1:
+输入: J = "aA", S = "aAAbbbb"
+输出: 3
+
+示例 2:
+输入: J = "z", S = "ZZ"
+输出: 0
+
+注意:
+S 和 J 最多含有50个字母。
+J 中的字符不重复。
+"""
+
+
+THINKING = """
+遍历J, 计数S中的总和
+"""
+
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        return sum(S.count(jewel) for jewel in J)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    J = "aA"
+    S = "aAAbbbb"
+    print(s.numJewelsInStones(J, S))
