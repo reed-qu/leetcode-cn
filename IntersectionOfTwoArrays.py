034de2870e5f8c6d3edb5b448f813f9ff924f5e4
reedwave <reed_qu@qq.com>
2020-01-11 11:59:48 +0800

    349. 两个数组的交集 | #91

---
+++ IntersectionOfTwoArrays.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/11 上午11:54
+# @Title   : 349. 两个数组的交集
+# @Link    : https://leetcode-cn.com/problems/intersection-of-two-arrays/
+
+
+QUESTION = """
+给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1:
+输入: nums1 = [1,2,2,1], nums2 = [2,2]
+输出: [2]
+
+示例 2:
+输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出: [9,4]
+
+说明:
+输出结果中的每个元素一定是唯一的。
+我们可以不考虑输出结果的顺序。
+"""
+
+
+THINKING = """
+so easy
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums1 = [4,9,5]
+    nums2 = [9,4,9,8,4]
+    print(s.intersection(nums1, nums2))
